63b29dfa8fe5f30fbab4d1e0ef91a7be27577614
eggplants <w10776e8w@yahoo.co.jp>
2022-01-03 16:57:20 +0900

    add: query script

---
+++ q.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+USAGE="usage: $0 query format"
+ENDPOINT="https://query.wikidata.org/sparql"
+
+_exec_q(){
+  curl "$ENDPOINT" \
+    -H "$1" \
+    -X POST \
+    --data-urlencode query@/dev/stdin
+}
+
+main(){
+  if [ $# != '2' ]; then
+    echo "$USAGE" >&2
+    return 1
+  fi
+  q="$1"
+  format="$2"
+
+  if [ "$q" = '-' ]; then
+    q='/dev/stdin'
+  elif ! [ -f "$q" ]; then
+    echo "error: file '$q' is not found">&2
+    return 1
+  fi
+
+  case "$format" in
+    "XML" | "xml" )
+      format='application/sparql-results+xml'
+      ;;
+    "JSON" | "json" )
+      format='application/sparql-results+json'
+      ;;
+    "TSV" | "tsv" )
+      format='text/tab-separated-values'
+      ;;
+    "CSV" | "csv" )
+      format='text/csv'
+      ;;
+    "RDF" | "rdf" | "BINARY" | "binary" )
+      foramt='application/x-binary-rdf-results-table'
+      ;;
+    *)
+      echo 'error: invalid format '"$format" >&2
+      return 1
+      ;;
+  esac
+
+  cat "$1" | _exec_q "Accept: $format"
+}
+main "${@}"
+exit $?
